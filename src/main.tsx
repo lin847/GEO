d1b1471c09efe57c2d5988d99600bd6a211dba02
lin847 <lkwebd1@gmail.com>
2026-05-28 15:51:19 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
-function navigation()
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
